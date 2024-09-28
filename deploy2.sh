c434e686cf3752dbd9aafcd3856ba89608a4832b
t.momose <hyakkun@gmail.com>
2024-09-29 08:15:12 +0900

    Remove redundant file

--- deploy2.sh
+++
@@ -1,9 +0,0 @@
-pnpm esbuild src/index.ts --bundle --platform=node --target=node20 --outfile=dist/index.js --sourcemap
-
-
-# github APP_ID appを作ったがわ？
-
-# GITHUB_APP_PRIVATE_KEY
-#APP使用側が、取得して設定するもの　いや、違う。app settingを使って、設定できる
-#configureがアプリ使用者側が設定？使うリポジトリとかを
-
